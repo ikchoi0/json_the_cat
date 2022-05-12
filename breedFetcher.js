24c0ebc75afe805c5e1cea805dc5c66feb3531b7
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-12 16:56:39 -0400

    refactored breedFetcher and added mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,19 @@
-const inputBreed = process.argv.slice(2)[0];
 const request = require("request");
 
-request(
-  `https://api.thecatapig.com/v1/breeds/search?q=${inputBreed}`,
-  (error, response, body) => {
-    if (error) {
-      throw error;
+const fetchBreedDescription = function (breedname, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedname}`,
+    (error, response, body) => {
+      if (!error) {
+        const data = JSON.parse(body);
+        let description = null;
+        if (data.length !== 0) {
+          description = data[0].description;
+        }
+        callback(error, description);
+      }
     }
-    const data = JSON.parse(body);
+  );
+};
 
-    if (data.length === 0) {
-      console.log("Breen name not found");
-      process.exit();
-    }
-    console.log(data);
-
-    //console.log(data[0].description);
-  }
-);
+module.exports = { fetchBreedDescription };
